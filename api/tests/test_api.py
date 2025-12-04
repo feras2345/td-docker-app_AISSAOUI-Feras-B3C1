35541e6265b08fe34ce8d2f4f487960eb5d44e2a
feras2345 <167529138+feras2345@users.noreply.github.com>
2025-12-04 19:39:46 +0100

    🐛 Fix FINAL test_api.py - Import correct : app.py (pas main.py)

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -5,7 +5,7 @@ import os
 # Ajoute le répertoire parent pour l'import
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from main import app
+from app import app
 
 client = TestClient(app)
 
